69b55aa9d2d49d7002a5f46c91bf4f4c2b327558
l1npengtul <l1npengtul@protonmail.com>
2026-05-06 00:40:55 +0900

    add rust analyzer

--- flake.nix
+++ flake.nix
@@ -5,25 +5,33 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    rust-overlay,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [rust-overlay.overlays.default];
+          overlays = [ rust-overlay.overlays.default ];
           config.allowUnfree = true;
         };
-        rustbin = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+        rustbin = pkgs.rust-bin.selectLatestNightlyWith (
+          toolchain:
           toolchain.default.override {
-            extensions = ["rust-src"];
-          });
-      in {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+            ];
+          }
+        );
+      in
+      {
         formatter = pkgs.alejandra;
 
         packages.default = pkgs.rustPlatform.buildRustPackage {
@@ -37,35 +45,36 @@
           };
         };
 
-        nixosModules.default = {
-          config,
-          options,
-          ...
-        }: {
-          # TODO: Systemd Service Module
-        };
+        nixosModules.default =
+          {
+            config,
+            options,
+            ...
+          }:
+          {
+            # TODO: Systemd Service Module
+          };
 
         devShells.default = pkgs.mkShell {
-          packages =
-            [
-              rustbin
-            ]
-            ++ (with pkgs; [
-              llvmPackages.libclang.lib
-              llvmPackages.clang
-              pkg-config
-              cmake
-              vcpkg
-              rustPlatform.bindgenHook
-              rustup
-              simple-http-server
-              http-server
-              esbuild
-              lldb
-              qrrs
-              qrencode
-              sqlx-cli
-            ]);
+          packages = [
+            rustbin
+          ]
+          ++ (with pkgs; [
+            llvmPackages.libclang.lib
+            llvmPackages.clang
+            pkg-config
+            cmake
+            vcpkg
+            rustPlatform.bindgenHook
+            rustup
+            simple-http-server
+            http-server
+            esbuild
+            lldb
+            qrrs
+            qrencode
+            sqlx-cli
+          ]);
 
           env.RUST_SRC_PATH = "${rustbin}/lib/rustlib/src/rust/library";
           env.LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
